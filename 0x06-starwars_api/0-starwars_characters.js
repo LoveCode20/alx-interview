57de4d94cc57a415a1a60e454af43fb31492fae2
LoveCode20 <amysweat20@gmail.com>
2024-07-26 01:14:29 +0100

    fixes

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,28 @@
 #!/usr/bin/node
 
-const request = require('request');
+const request = require("request");
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
+const movieNum = process.argv[2];
+const URL = `https://swapi-api.alx-tools.com/api/films/${movieNum}`;
+
+// Makes API request, sets async to allow await promise
+request(URL, async function (err, res, body) {
+  if (err) return console.error(err);
+
+  // parse each character in the film as a list objects
+  const charURLList = JSON.parse(body).characters;
+
+  // Use URL list to character pages to make new requests
+  // await queues requests until they resolve in order
+  for (const charURL of charURLList) {
+    await new Promise(function (resolve, reject) {
+      request(charURL, function (err, res, body) {
+        if (err) return console.error(err);
+
+        // finds each character name and prints in URL order
+        console.log(JSON.parse(body).name);
+        resolve();
+      });
+    });
+  }
 });
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
-  });
-};
